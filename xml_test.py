9094fe66c88dba9f5698f31d2465b89f8635b209
Raidfire-SDR <125891383+Raidfire-SDR@users.noreply.github.com>
2024-03-19 00:20:11 +0200

    Add files via upload

--- xml_test.py
+++ xml_test.py
@@ -11,10 +11,9 @@
 
 from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import fft
-from gnuradio.fft import window
 from gnuradio import gr
 from gnuradio.filter import firdes
+from gnuradio.fft import window
 import sys
 import signal
 from argparse import ArgumentParser
@@ -58,7 +57,7 @@ class xml_test(gr.top_block):
         self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
         self.xmlrpc_server_0_thread.daemon = True
         self.xmlrpc_server_0_thread.start()
-        self.network_tcp_sink_3 = network.tcp_sink(gr.sizeof_float, 1, '127.0.0.1', 6001,2)
+        self.network_tcp_sink_3 = network.tcp_sink(gr.sizeof_gr_complex, 1, '127.0.0.1', 6001,2)
         self.iio_pluto_source_0 = iio.fmcomms2_source_fc32('ip:192.168.2.1' if 'ip:192.168.2.1' else iio.get_pluto_uri(), [True, True], 32768)
         self.iio_pluto_source_0.set_len_tag_key('packet_len')
         self.iio_pluto_source_0.set_frequency(lop)
@@ -76,12 +75,7 @@ class xml_test(gr.top_block):
         self.iio_pluto_sink_1.set_samplerate(samp_rate)
         self.iio_pluto_sink_1.set_attenuation(0, 0)
         self.iio_pluto_sink_1.set_filter_params('Auto', '', 0, 0)
-        self.fft_vxx_0_0_0_1 = fft.fft_vcc(fft_size, True, window.blackmanharris(fft_size), False, 8)
-        self.blocks_vector_to_stream_0_0 = blocks.vector_to_stream(gr.sizeof_float*1, fft_size)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
-        self.blocks_stream_to_vector_0_1 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, fft_size)
         self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
-        self.blocks_complex_to_mag_0_0 = blocks.complex_to_mag(fft_size)
         self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 10000, 1, 0.01, 0)
         self.analog_agc_xx_0_0 = analog.agc_cc((1e-4), 1.0, 1.0)
         self.analog_agc_xx_0_0.set_max_gain(65536)
@@ -96,12 +90,7 @@ class xml_test(gr.top_block):
         self.connect((self.analog_agc_xx_0_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.analog_agc_xx_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.iio_pluto_sink_1, 0))
-        self.connect((self.blocks_complex_to_mag_0_0, 0), (self.blocks_vector_to_stream_0_0, 0))
-        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.blocks_stream_to_vector_0_1, 0))
-        self.connect((self.blocks_stream_to_vector_0_1, 0), (self.fft_vxx_0_0_0_1, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.network_tcp_sink_3, 0))
-        self.connect((self.blocks_vector_to_stream_0_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.fft_vxx_0_0_0_1, 0), (self.blocks_complex_to_mag_0_0, 0))
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.network_tcp_sink_3, 0))
         self.connect((self.iio_pluto_source_0, 0), (self.analog_agc_xx_0_0, 0))
 
 
@@ -138,7 +127,6 @@ class xml_test(gr.top_block):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.iio_pluto_sink_1.set_samplerate(self.samp_rate)
         self.iio_pluto_source_0.set_samplerate(self.samp_rate)
 
