df93c3b8041d18b51533fa5f55db7936e3596693
raidfire <jason.holder1975@gmail.com>
2024-03-21 01:35:29 +0200

    New

--- radar_rx_epy_module_0.py
+++ radar_rx_epy_module_0.py
@@ -1,7 +1,7 @@
 # this module will be imported in the into your flowgraph
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 GPIO.output(18, 1)
 
