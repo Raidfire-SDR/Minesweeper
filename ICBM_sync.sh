c97fa973f018bff07a2c9e94da858293b25a8805
raidfire <jason.holder1975@gmail.com>
2024-03-21 14:57:44 +0200

    New

--- ICBM_sync.sh
+++ ICBM_sync.sh
@@ -2,4 +2,4 @@
 git commit -m New
 git push -u -f origin
 
-ssh 192.168.1.105   cd ~/Minesweeper & git pull --all 
+ssh 192.168.1.105 'rm -r -f tmp & git clone https://github.com/Raidfire-SDR/Minesweeper.git tmp & cp -f ~/tmp/*.* ~/Minesweeper/*.*'
